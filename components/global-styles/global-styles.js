5690c59244164c6d44df1dd24b132f13825ad726
Jeremias Menichelli <jmenichelli@newrelic.com>
2020-04-13 03:35:12 +0200

    chore: add spaces after font families

--- components/global-styles/global-styles.js
+++ components/global-styles/global-styles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
     body {
-        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
         margin: 0;
     }
 `
