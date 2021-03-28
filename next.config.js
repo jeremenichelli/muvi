c27b63c2f0fdae0405686e7db590691ca190bdf7
Jere Menichelli <jmenichelli@gmail.com>
2021-03-28 13:01:09 +0200

    chore(Build): Update preact configuration, add analyze command

--- next.config.js
+++ next.config.js
@@ -1,35 +1,20 @@
 require('dotenv').config()
+const withPlugins = require('next-compose-plugins')
+const withPreact = require('next-plugin-preact')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = {
-  env: {
-    API_KEY: process.env.API_KEY
-  },
-
-  experimental: {
-    modern: true,
-    polyfillsOptimization: true
-  },
-
-  webpack(config) {
-    const splitChunks = config.optimization && config.optimization.splitChunks
-
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules
-        })
-        cacheGroups.commons.name = 'framework'
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules
-        }
-      }
-    }
-
-    return config
-  }
+const env = {
+  API_KEY: process.env.API_KEY
 }
+
+module.exports = withPlugins([
+  // plugins
+  withPreact,
+  withBundleAnalyzer,
+  // custom Next configuration
+  {
+    env
+  }
+])
